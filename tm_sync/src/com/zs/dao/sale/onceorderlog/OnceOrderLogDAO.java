8466bb5527c69203048d62507b68a400cde05286
Allen <2319772333@qq.com>
2016-10-12 17:05:46 +0800

    *

--- tm_sync/src/com/zs/dao/sale/onceorderlog/OnceOrderLogDAO.java
+++ tm_sync/src/com/zs/dao/sale/onceorderlog/OnceOrderLogDAO.java
@@ -10,8 +10,8 @@ import org.springframework.data.jpa.repository.Query;
  */
 public interface OnceOrderLogDAO extends EntityJpaDao<StudentBookOnceOrderLog, Long> {
     @Modifying
-    @Query(nativeQuery = true, value = "INSERT into student_book_once_order_log(order_code, state, operator, operate_time) " +
-            "select DISTINCT sbo.order_code, 6, '管理员', now() " +
+    @Query(nativeQuery = true, value = "INSERT into student_book_once_order_log(order_id, state, operator, operate_time) " +
+            "select DISTINCT sbo.id, 6, '管理员', now() " +
             "from student_book_once_order sbo, student_book_order_package sbop " +
             "where sbo.package_id = sbop.id and sbop.logistic_code= ?1")
     public void addSignOrderLog(String nu);
